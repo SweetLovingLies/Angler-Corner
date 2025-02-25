5fc36ecd08ac838aeb825c922806aab416994e9f
Hana Harris <Selkiefawn@gmail.com>
2025-02-25 12:37:13 -0500

    Initial Commit

---
+++ FishingLeaderboard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FishingLeaderboard
+//
+//  Created by Morgan Harris on 2/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FishingLeaderboard/FishingLeaderboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  FishingLeaderboardApp.swift
+//  FishingLeaderboard
+//
+//  Created by Morgan Harris on 2/25/25.
+//
+
+import SwiftUI
+
+@main
+struct FishingLeaderboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
